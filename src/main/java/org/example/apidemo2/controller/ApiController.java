2d2b2b5d9b5338ef18508ac8b7f68334502aa9d0
canarmagan <canarmgn@gmail.com>
2024-05-15 10:52:58 +0300

    getHeadline method has been added

--- src/main/java/org/example/apidemo2/controller/ApiController.java
+++ src/main/java/org/example/apidemo2/controller/ApiController.java
@@ -20,31 +20,6 @@ public class ApiController {
 
     @GetMapping("/getapidata")
     public String getData() {
-        String responseData = apiService.getData();
-        if (responseData.equals("API'den veri alınamadı.")) {
-            return responseData;
-        }
-
-        try {
-            JSONObject jsonResponse = new JSONObject(responseData);
-            JSONArray docsArray = jsonResponse.getJSONObject("response").getJSONArray("docs");
-
-            for (int i = 0; i < docsArray.length(); i++) {
-                JSONObject doc = docsArray.getJSONObject(i);
-                String webUrl = doc.getString("web_url");
-                JSONObject headline = doc.getJSONObject("headline");
-                String mainHeadline = headline.getString("main");
-
-                Article article = new Article();
-                article.setWebUrl(webUrl);
-                article.setHeadline(mainHeadline);
-                articleRepository.save(article);
-            }
-
-            return "Veriler başarıyla kaydedildi.";
-        } catch (Exception e) {
-            e.printStackTrace();
-            return "Veri işlenirken bir hata oluştu.";
-        }
+        return apiService.getHeadline();
     }
 }
--- src/main/java/org/example/apidemo2/service/ApiService.java
+++ src/main/java/org/example/apidemo2/service/ApiService.java
@@ -1,13 +1,21 @@
 package org.example.apidemo2.service;
 
+import org.example.apidemo2.apiRespository.ArticleRepository;
+import org.example.apidemo2.model.Article;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class ApiService {
 
-
     private static final String APİ_KEY = "gnRpWLDOtlL3sOGKnhXNRx4Tqny9cxNR";
+    public final ArticleRepository articleRepository;
+
+    public ApiService(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
 
     public String getData() {
         try {
@@ -20,4 +28,34 @@ public class ApiService {
             return "API'den veri alınamadı.";
         }
     }
+
+    public String getHeadline() {
+        String responseData = getData();
+        if (responseData.equals("API'den veri alınamadı.")) {
+            return responseData;
+        }
+
+        try {
+            JSONObject jsonResponse = new JSONObject(responseData);
+            JSONArray docsArray = jsonResponse.getJSONObject("response").getJSONArray("docs");
+
+            for (int i = 0; i < docsArray.length(); i++) {
+                JSONObject doc = docsArray.getJSONObject(i);
+                String webUrl = doc.getString("web_url");
+                JSONObject headline = doc.getJSONObject("headline");
+                String mainHeadline = headline.getString("main");
+
+                Article article = new Article();
+                article.setWebUrl(webUrl);
+                article.setHeadline(mainHeadline);
+                articleRepository.save(article);
+            }
+
+            return "Veriler başarıyla kaydedildi.";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Veri işlenirken bir hata oluştu.";
+        }
+    }
+
 }
